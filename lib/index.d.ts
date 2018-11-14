3d48ea99af61334680a33d669b06f19fa4c70692
Hailey Keen <haileyckeen@gmail.com>
2018-11-14 18:23:53 -0500

    Rough draft of typescript declarations for lib/

---
+++ lib/index.d.ts
@@ -0,0 +1,35 @@
+declare namespace Hydra {
+
+  // TODO: make a typed ConfigProps object for config
+  export class Config {
+    constructor();
+
+    config: {};
+
+    getObject(): {};
+    init(cfg: {}): Promise<void>;
+  }
+
+  export class Cache<T> {
+    constructor();
+
+    data: {[key: string]: T};
+
+    put(key: string, value: T, expiration: number): void;
+    get(key: string): T;
+  }
+
+  // TODO: make a typed OptionsProps object for options
+  export class RedisConnection {
+    constructor(redisConfig: {}, defaultRedisDb: number, testMode: boolean);
+
+    getRedis(): any; // TODO: return a Redis constructor
+    connect(options: {}): void;
+    attempt(action: () => Promise<any>): void;
+  }
+
+  // TODO: server-request
+  // TODO: server-response
+  // TODO: umfmessage
+  // TODO: utils
+}
