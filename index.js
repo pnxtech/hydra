bef363c8dac0cc5a1227eb6e86182941477c29ef
Eric Adum <eric.adum@gmail.com>
2019-03-27 13:17:02 -0400

    pass options/callback args through to redis duplicate call (#209)

--- index.js
+++ index.js
@@ -1680,10 +1680,12 @@ class Hydra extends EventEmitter {
   /**
   * @name _getClonedRedisClient
   * @summary get a Redis client connection which points to the same Redis server that hydra is using
+  * @param {object} [options] - override options from original createClient call
+  * @param {function} [callback] - callback for async connect
   * @return {object} - Redis Client
   */
-  _getClonedRedisClient() {
-    return this.redisdb.duplicate();
+  _getClonedRedisClient(options, callback) {
+    return this.redisdb.duplicate(options, callback);
   }
 
   /**
